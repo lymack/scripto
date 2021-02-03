ee9fcacd2f82c94586b2f50b8d49f17ee79b67c6
lymack <lydiam@hey.com>
2021-02-03 16:43:56 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+# This script will install a new BookStack instance on a fresh Ubuntu 20.04 server.
+# This script is experimental and does not ensure any security.
+
+# Fetch domain to use from first provided parameter,
+# Otherwise request the user to input their domain
+DOMAIN=$1
+if [ -z $1 ]
+then
+echo ""
+printf "Enter the domain you want to host BookStack and press [ENTER]\nExamples: my-site.com or docs.my-site.com\n"
+read DOMAIN
+fi
+
+# Get the current machine IP address
+CURRENT_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+# Install core system packages
+export DEBIAN_FRONTEND=noninteractive
+add-apt-repository universe
+apt update
+apt install -y git unzip php7.4 curl php7.4-fpm php7.4-curl php7.4-mbstring php7.4-ldap \
+php7.4-tidy php7.4-xml php7.4-zip php7.4-gd php7.4-mysql mysql-server-8.0 libapache2-mod-php7.4
+
+# Set up database
+DB_PASS="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
+mysql -u root --execute="CREATE DATABASE bookstack;"
+mysql -u root --execute="CREATE USER 'bookstack'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASS';"
+mysql -u root --execute="GRANT ALL ON bookstack.* TO 'bookstack'@'localhost';FLUSH PRIVILEGES;"
+
+# Download BookStack
+cd /var/www
+git clone https://github.com/BookStackApp/BookStack.git --branch release --single-branch bookstack
+BOOKSTACK_DIR="/var/www/bookstack"
+cd $BOOKSTACK_DIR
+
+# Install composer
+EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+curl -s https://getcomposer.org/installer > composer-setup.php
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
+then
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+else
+    >&2 echo 'ERROR: Invalid composer installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+# Install BookStack composer dependencies
+export COMPOSER_ALLOW_SUPERUSER=1
+php composer.phar install --no-dev --no-plugins
+
+# Copy and update BookStack environment variables
+cp .env.example .env
+sed -i.bak "s@APP_URL=.*\$@APP_URL=http://$DOMAIN@" .env
+sed -i.bak 's/DB_DATABASE=.*$/DB_DATABASE=bookstack/' .env
+sed -i.bak 's/DB_USERNAME=.*$/DB_USERNAME=bookstack/' .env
+sed -i.bak "s/DB_PASSWORD=.*\$/DB_PASSWORD=$DB_PASS/" .env
+
+# Generate the application key
+php artisan key:generate --no-interaction --force
+# Migrate the databases
+php artisan migrate --no-interaction --force
+
+# Set file and folder permissions
+chown www-data:www-data -R bootstrap/cache public/uploads storage && chmod -R 755 bootstrap/cache public/uploads storage
+
+echo ""
+echo "Setup Finished, Your BookStack instance should now be installed."
+echo "You can login with the email 'admin@admin.com' and password of 'password'"
+echo "MySQL was installed without a root password, It is recommended that you set a root MySQL password."
+echo ""
+echo "You can access your BookStack instance at: http://$CURRENT_IP/ or http://$DOMAIN/"
